8132d1d9842e2fe671a69a25eef590cf8d8bc1ca
h4rdl0ckl3z <bank427ingza@gmail.com>
2024-11-24 18:19:57 +0700

    test commit

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  },
+  output: "export",
+  reactStrictMode: true,
 };
 
 export default nextConfig;
