7766d9eb14f6766863167d87d87e7bae93df8c82
Avatart0Dev <54482333+h4rdl0ckl3z@users.noreply.github.com>
2024-11-24 17:58:04 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  output: 'dist',
+  output: 'export',
+  distDir: 'dist',
+  images: {
+    unpotimized: true,
+  },
 };
 
 export default nextConfig;
