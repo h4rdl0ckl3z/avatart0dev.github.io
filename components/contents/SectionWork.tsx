713c5ca1b2218cef4138d736297c334e79ba9bd9
h4rdl0ckl3z <bank427ingza@gmail.com>
2024-11-27 13:10:57 +0700

    sectionWork line

--- components/contents/SectionWork.tsx
+++ components/contents/SectionWork.tsx
@@ -1,18 +1,30 @@
+"use client";
+
+import { useTheme } from "next-themes";
+import { useState, useEffect } from 'react'
+
 export const SectionWork = () => {
+    const { theme } = useTheme();
+    const [isClient, setIsClient] = useState(false)
+    useEffect(() => {
+        setIsClient(true)
+    }, [])
+    const border = theme === 'light' ? 'border-black' : 'border-white';
+    const bg = theme === 'light' ? 'bg-black' : 'bg-white';
     return (
         <section id="work" className="py-20">
             <div className="container mx-auto text-center">
                 <h2 className="text-3xl font-bold text-center mb-4">Work</h2>
             </div>
             <div className="relative w-4/4 py-4 pr-4">
-                <div className="absolute left-1/2 transform -translate-x-1/2 h-full border-l-2 border-black"></div>
+                <div className={isClient ? `absolute left-1/2 transform -translate-x-1/2 h-full border-l-2 ${border}` : ''}></div>
 
                 <div className="flex items-center mb-8">
                     <div className="w-1/2 text-right pr-8">
                         <p className="text-lg">2024</p>
                     </div>
                     <div className="relative w-1/2">
-                        <div className="absolute left-0 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-black rounded-full"></div>
+                        <div className={isClient ? `absolute left-0 top-1/2 transform -translate-y-1/2 w-4 h-4 ${bg} rounded-full` : ''}></div>
                         <div className="ml-8">
                             <p className="mb-4">Lorem ipsum dolor sit amet.</p>
                             <p className="mb-4">Consectetur adipiscing elit.</p>
@@ -27,7 +39,7 @@ export const SectionWork = () => {
                         <p className="text-lg">2023</p>
                     </div>
                     <div className="relative w-1/2">
-                        <div className="absolute left-0 top-1/2 transform -translate-y-1/2 w-4 h-4 bg-black rounded-full"></div>
+                        <div className={isClient ? `absolute left-0 top-1/2 transform -translate-y-1/2 w-4 h-4 ${bg} rounded-full` : ''}></div>
                         <div className="ml-8">
                             <p className="mb-4">Lorem ipsum dolor sit amet.</p>
                             <p className="mb-4">Consectetur adipiscing elit.</p>
