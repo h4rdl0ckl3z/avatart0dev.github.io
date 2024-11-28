929259067c1d310431a5ff9700bcfe4a27666e71
h4rdl0ckl3z <bank427ingza@gmail.com>
2024-11-28 18:15:54 +0700

    use /portfolio default

--- components/contents/SectionProJect.tsx
+++ components/contents/SectionProJect.tsx
@@ -4,7 +4,7 @@ import { promises as fs } from 'fs';
 export const SectionProject = async () => {
     const file = await fs.readFile(process.cwd() + '/app/project/data.json', 'utf8');
     const data: { data: Project[] } = JSON.parse(file);
-
+    //push project on github path /portfolio default
     return (
         <section id="projects" className="py-20">
             <div className="container mx-auto text-center">
@@ -15,7 +15,7 @@ export const SectionProject = async () => {
                             <img src={project.img} width={200} height={300} alt={`Screenshot of ${project.title}`} className="mb-4 rounded" />
                             <h3 className="text-xl font-bold mb-2">{project.title}</h3>
                             <p className="mb-4">{project.description}</p>
-                            {project.link && <a href={`/project/${index}`} className="text-blue-500 hover:underline">View Project</a>}
+                            {project.link && <a href={`/portfolio/project/${index}`} className="text-blue-500 hover:underline">View Project</a>}
                         </div>
                     ))}
                 </div>
