252c20da994528d259182f987a25d35e7c4ee8f8
h4rdl0ckl3z <bank427ingza@gmail.com>
2024-11-29 13:52:48 +0700

    aboutme and logo project

--- components/contents/SectionAbout.tsx
+++ components/contents/SectionAbout.tsx
@@ -3,7 +3,9 @@ export const SectionAbout = () => {
         <section id="about" className="py-20">
             <div className="container mx-auto text-center">
                 <h2 className="text-3xl font-bold mb-4">About Me</h2>
-                <p className="text-xl mb-8">I am a passionate developer with experience in React and Tailwind CSS. I love creating beautiful and functional web applications.</p>
+                <p className="text-xl mb-8">
+                    I'm a qualifi and skill developer with learning and experience. I always code with an eye for detail. I'm readly to learn new thing and develop myself.
+                </p>
                 <img src="https://avatars.githubusercontent.com/u/54482333?s=400&u=c430a54568014a47938386f3aa960d7cb97f9125&v=4" width={200} height={200} alt="A detailed portrait of the developer" className="mx-auto rounded-full" />
             </div>
         </section>
